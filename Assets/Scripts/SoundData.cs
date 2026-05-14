588b5ab715420ca923432de2704ac702b9a54c4f
Jorge Fernando Lopez <jorgefernandolope117@gmail.com>
2026-05-13 20:39:06 -0600

    DanceHero_2

---
+++ Assets/Scripts/SoundData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SongData", menuName = "Scriptable Objects/SoundData")]
+public class SongData : ScriptableObject
+{
+        public string animationName;
+        public string songName;
+    
+}
